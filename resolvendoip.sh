74ac52aff3edea60fe8888ccdaa6c1fbd555b2c7
michaelklaan <40411471+michaelklaan@users.noreply.github.com>
2021-12-21 14:50:11 -0300

    newversion
    sem pular linha

--- resolvendoip.sh
+++ resolvendoip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for ip in $(cat listadeip.txt);
+for ip in $(cat ips.txt);
 do
-echo "$ip";
-curl -X GET http://ip-api.com/json/$ip --silent | cut -d '"' -f 8,20,24,44,40 | sed 's/"/ /' | sed 's/"/ /' | sed 's/"/ /' | sed 's/"/ /' | sed 's/ org/ /';
+echo -n "$ip ";
+curl -X GET http://ip-api.com/json/$ip --silent | cut -d '"' -f 8,20,24,44,40 | sed 's/"/ /' | sed 's/"/ /' | sed 's/"/ /' | sed 's/"/ /' | sed 's/ org/ /'; sleep 1;
 done
